b73b217162b10bb4f60095f036f15acbf40cc686
Vladislav Zinger <vladislavzinger@gmail.com>
2023-01-21 14:03:33 +0700

    added hello world project

---
+++ demo_hello_world/demo_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_hello_worldApp.swift
+//  demo_hello_world
+//
+//  Created by runner on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct demo_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
